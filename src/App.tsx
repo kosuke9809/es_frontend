bc6274453dd64f5785644f56d1d5c9a199c447c8
kosuke9809 <uht0912k@outlook.com>
2021-10-30 16:17:29 +0900

    add header & Top page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
 import React from "react";
 import "./App.css";
+import { Container, TextField, Button } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const SubmitButton = styled(Button)({
+  width: "100%",
+});
 
 const App: React.VFC = () => {
-  return <div className='App'>テスト</div>;
+  return (
+    <Container fixed>
+      <h1>AIでESを自動採点</h1>
+      <div>
+        <h3>アピールしたい長所を入力してください</h3>
+        <TextField fullWidth placeholder='向上心' />
+        <h3>あなたの長所は教えて下さい</h3>
+        <TextField
+          fullWidth
+          multiline
+          rows={10}
+          placeholder='私の強みは常に高みを目指して努力する向上心があるところです。
+        前職では営業をしておりましたが、入社して1年が経っても成績が上がることはありませんでした。
+        何かやり方が間違っているのか、ほかに売り上げを上げる方法があるのではないかと試行錯誤した結果、自分の欠点が見えてきたのです。
+        営業成績を上げるのは単なる数字を追うだけではなく、お客様の立場に立ってきちんと話を聞くことや、スピード感を持って対応することなど、営業の基本を学び直したことで、さまざまなことを得ることができました。
+        それからは、営業成績も上がっていき、部署内で優秀賞をいただくことができました。
+        今後も、現状に満足することなく高みを目指していきたいと考えています。
+        '
+        />
+      </div>
+      <br />
+      <div className='submit_btn'>
+        <SubmitButton variant='outlined'>採点</SubmitButton>
+      </div>
+    </Container>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Header from "./components/Header";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <div className='home'>
+        <Header />
+        <App />
+      </div>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
